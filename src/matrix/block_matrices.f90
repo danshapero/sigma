dd62ce435b51909d0014ad969d0fa15c6a97bcb2
danshapero <shapero.daniel@gmail.com>
2013-09-26 11:45:25 -0700

    Started writing block matrix and multi-vector data types

---
+++ src/matrix/block_matrices.f90
@@ -0,0 +1,283 @@
+module block_matrices
+
+use types
+use sparse_matrices
+
+implicit none
+
+
+!--------------------------------------------------------------------------!
+type, extends(sparse_matrix) :: block_matrix                               !
+!--------------------------------------------------------------------------!
+    type(sparse_matrix_pointer), allocatable :: mats(:,:)
+    integer :: mat_rows, mat_cols
+    integer, allocatable :: row_ptr(:), col_ptr(:)
+    real(dp), allocatable :: z(:)
+
+contains
+    ! procedures required by virtue of inheritance from abstract
+    ! sparse_matrix data type
+    procedure :: init => block_mat_init
+    procedure :: neighbors => block_mat_neighbors
+    procedure :: get_value => block_mat_get_value
+    procedure :: set_value => block_mat_set_value
+    procedure :: add_value => block_mat_add_value
+    procedure :: sub_matrix_add => block_mat_sub_matrix_add
+    procedure :: left_permute => block_mat_left_permute
+    procedure :: right_permute => block_mat_right_permute
+    procedure :: matvec => block_mat_matvec
+    procedure :: matvec_t => block_mat_matvec_t
+
+    ! procedures specific to block matrices
+    procedure :: get_block
+    procedure :: set_block
+end type block_matrix
+
+
+
+contains
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_init(A,nrow,ncol,orientation,g)                       !
+!--------------------------------------------------------------------------!
+    class(block_matrix), intent(inout) :: A
+    integer, intent(in) :: nrow,ncol
+    character(len=3), intent(in) :: orientation
+    class(graph), pointer, intent(in), optional :: g
+
+    A%nrow = nrow
+    A%ncol = ncol
+    A%orientation = orientation
+
+    allocate(A%z(max(nrow,ncol)))
+
+end subroutine block_mat_init
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_neighbors(A,i,nbrs)                                   !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(in) :: A
+    integer, intent(in)  :: i
+    integer, intent(out) :: nbrs(:)
+    ! local variables
+    integer :: j
+
+    nbrs = 0
+    
+end subroutine block_mat_neighbors
+
+
+
+!--------------------------------------------------------------------------!
+function block_mat_get_value(A,i,j)                                        !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(in) :: A
+    integer, intent(in) :: i,j
+    real(dp) :: block_mat_get_value
+    ! local variables
+    integer :: k,l,m,n
+
+    block_mat_get_value = 0.0_dp
+
+    ! Find which block (k,l) contains the global entry (i,j)
+    do k=1,A%mat_rows-1
+        if (A%row_ptr(k)<=i .and. A%row_ptr(k+1)>i) exit
+    enddo
+
+    do l=1,A%mat_cols-1
+        if (A%col_ptr(l)<=j .and. A%col_ptr(k+1)>j) exit
+    enddo
+
+    ! Local index of entry (i,j) in sub-matrix (k,l) 
+    m = i-A%row_ptr(k)+1
+    n = j-A%col_ptr(l)+1
+
+    block_mat_get_value = A%mats(k,l)%get_value(m,n)
+
+end function block_mat_get_value
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_set_value(A,i,j,val)                                  !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(inout) :: A
+    integer, intent(in) :: i,j
+    real(dp), intent(in) :: val
+    ! local variables
+    integer :: k,l,m,n
+
+    do k=1,A%mat_rows-1
+        if (A%row_ptr(k)<=i .and. A%row_ptr(k+1)>i) exit
+    enddo
+
+    do l=1,A%mat_cols-1
+        if (A%col_ptr(l)<=j .and. A%col_ptr(k+1)>j) exit
+    enddo
+
+    m = i-A%row_ptr(k)+1
+    n = j-A%col_ptr(l)+1
+
+    call A%mats(k,l)%set_value(m,n,val)
+
+end subroutine block_mat_set_value
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_add_value(A,i,j,val)                                  !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(inout) :: A
+    integer, intent(in) :: i,j
+    real(dp), intent(in) :: val
+    ! local variables
+    integer :: k,l,m,n
+
+    do k=1,A%mat_rows-1
+        if (A%row_ptr(k)<=i .and. A%row_ptr(k+1)>i) exit
+    enddo
+
+    do l=1,A%mat_cols-1
+        if (A%col_ptr(l)<=j .and. A%col_ptr(k+1)>j) exit
+    enddo
+
+    m = i-A%row_ptr(k)+1
+    n = j-A%col_ptr(l)+1
+
+    call A%mats(k,l)%add_value(m,n,val)
+
+end subroutine block_mat_add_value
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_sub_matrix_add(A,B)                                   !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(inout) :: A
+    class(sparse_matrix), intent(in) :: B
+    ! local variables
+
+    ! Not quite sure how I'm going to play this one
+
+end subroutine block_mat_sub_matrix_add
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_left_permute(A,p)                                     !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(inout) :: A
+    integer, intent(in) :: p(:)
+    ! local variables
+
+    ! Or this
+
+end subroutine block_mat_left_permute
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_right_permute(A,p)                                    !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(inout) :: A
+    integer, intent(in) :: p(:)
+    ! local variables
+
+    ! Or this too
+
+end subroutine block_mat_right_permute
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_matvec(A,x,y)                                         !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(in) :: A
+    real(dp), intent(in)  :: x(:)
+    real(dp), intent(out) :: y(:)
+    ! local variables
+    integer :: i1,j1,i2,j2,k,l
+
+    do l=1,A%mat_cols
+        j1 = A%col_ptr(l)
+        j2 = A%col_ptr(l+1)-1
+
+        do k=1,A%mat_rows
+            i1 = A%row_ptr(k)
+            i2 = A%row_ptr(k+1)-1
+
+            call A%mats(k,l)%matvec(x(j1:j2),A%z(i1:i2))
+            y(i1:i2) = y(i1:i2)+z
+        enddo
+    enddo
+
+end subroutine block_mat_matvec
+
+
+
+!--------------------------------------------------------------------------!
+subroutine block_mat_matvec_t(A,x,y)                                       !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(block_matrix), intent(in) :: A
+    real(dp), intent(in)  :: x(:)
+    real(dp), intent(out) :: y(:)
+    ! local variables
+    integer :: i1,j1,i2,j2,k,l
+
+    do l=1,A%mat_cols
+        j1 = A%col_ptr(l)
+        j2 = A%col_ptr(l+1)-1
+
+        do k=1,A%mat_rows
+            i1 = A%row_ptr(k)
+            i2 = A%row_ptr(k+1)-1
+
+            call A%mats(k,l)%matvec_t(x(i1:i2),A%z(j1:j2))
+            y(j1:j2) = y(j1:j2)+z
+        enddo
+    enddo
+
+end subroutine block_mat_matvec_t
+
+
+
+!--------------------------------------------------------------------------!
+function get_block(A,k,l)                                                  !
+!--------------------------------------------------------------------------!
+    class(block_matrix), intent(in) :: A
+    integer, intent(in) :: k,l
+    class(sparse_matrix), pointer :: get_block
+
+    get_block => A%mats(k,l)
+
+end function get_block
+
+
+
+!--------------------------------------------------------------------------!
+subroutine set_block(A,B,k,l)                                              !
+!--------------------------------------------------------------------------!
+    class(block_matrix), intent(inout) :: A
+    class(sparse_matrix), pointer, intent(in) :: B
+    integer, intent(in) :: k,l
+
+    A%mats(k,l) => B
+
+end subroutine set_block
+
+
+
+end module block_matrices
---
+++ src/vectors.f90
@@ -0,0 +1,176 @@
+module vectors
+
+use types
+
+implicit none
+
+
+
+!--------------------------------------------------------------------------!
+type :: vector                                                             !
+!--------------------------------------------------------------------------!
+    real(dp), allocatable :: val(:)
+    integer :: nn, nc
+    integer, allocatable :: ptr(:)
+contains
+    generic :: init => init_vector, init_multi_vector
+    generic :: get_value => vec_get_value, vec_get_value_multi_index
+    generic :: set_value => vec_set_value, vec_set_value_multi_index
+    generic :: add_value => vec_add_value, vec_add_value_multi_index
+    procedure :: zero => vec_zero
+end type vector
+
+
+contains
+
+
+
+!--------------------------------------------------------------------------!
+subroutine init_vector(v,nn)                                               !
+!--------------------------------------------------------------------------!
+    type(vector), intent(inout) :: v
+    integer, intent(in) :: nn
+
+    v%nn = nn
+    v%nc = 1
+
+    allocate(v%val(nn),v%ptr(2))
+
+    v%val = 0.0_dp
+    v%ptr = [1, nn+1]
+
+end subroutine init_vector
+
+
+
+!--------------------------------------------------------------------------!
+subroutine init_multi_vector(v,nn_per_field)                               !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    type(vector), intent(inout) :: v
+    integer, intent(in) :: nn_per_field(:)
+    ! local variables
+    integer :: i
+
+    v%nn = sum(nn_per_field)
+    v%nc = size(nn_per_field)
+
+    allocate(v%val(v%nn),v%ptr(v%nc+1))
+    
+    v%val = 0.0_dp
+    v%ptr(1) = 1
+    do i=1,v%nc
+        v%ptr(i+1) = v%ptr(i)+nn_per_field(i)
+    enddo
+
+end subroutine init_multi_vector
+
+
+
+!--------------------------------------------------------------------------!
+function vec_get_value(v,i)                                                !
+!--------------------------------------------------------------------------!
+    type(vector), intent(in) :: v
+    integer, intent(in) :: i
+    real(dp) :: vec_get_value
+
+    vec_get_value = v%val(i)
+
+end function vec_get_value
+
+
+
+!--------------------------------------------------------------------------!
+function vec_get_value_multi_index(v,multi_index)                          !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    type(vector), intent(in) :: v
+    integer, intent(in) :: multi_index(2)
+    real(dp) :: vec_get_value_multi_index
+    ! local variables
+    integer :: i,k
+
+    i = multi_index(1)
+    k = multi_index(2)
+
+    vec_get_value_multi_index = v%val( v%ptr(k)+i-1 )
+
+end function vec_get_value_multi_index
+
+
+
+!--------------------------------------------------------------------------!
+subroutine vec_set_value(v,i,val)                                          !
+!--------------------------------------------------------------------------!
+    type(vector), intent(inout) :: v
+    integer, intent(in) :: i
+    real(dp), intent(in) :: val
+
+    v%val(i) = val
+
+end subroutine vec_set_value
+
+
+
+!--------------------------------------------------------------------------!
+subroutine vec_set_value_multi_index(v,multi_index,val)                    !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    type(vector), intent(inout) :: v
+    integer, intent(in) :: multi_index(2)
+    real(dp), intent(in) :: val
+    ! local variables
+    integer :: i,k
+
+    i = multi_index(1)
+    k = multi_index(2)
+
+    v%val( v%ptr(k)+i-1 ) = val
+
+end subroutine vec_set_value_multi_index
+
+
+
+!--------------------------------------------------------------------------!
+subroutine vec_add_value(v,i,val)                                          !
+!--------------------------------------------------------------------------!
+    type(vector), intent(inout) :: v
+    integer, intent(in) :: i
+    real(dp), intent(in) :: val
+
+    v%val(i) = v%val(i)+val
+
+end subroutine vec_add_value
+
+
+
+!--------------------------------------------------------------------------!
+subroutine vec_add_value_multi_index(v,multi_index,val)                    !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    type(vector), intent(inout) :: v
+    integer, intent(in) :: multi_index(2)
+    real(dp), intent(in) :: val
+    ! local variables
+    integer :: i,k
+
+    i = multi_index(1)
+    k = multi_index(2)
+
+    v%val( v%ptr(k)+i-1 ) = v%val( v%ptr(k)+i-1 )+val
+
+end subroutine vec_add_value_multi_index
+
+
+
+!--------------------------------------------------------------------------!
+subroutine vec_zero(v)                                                     !
+!--------------------------------------------------------------------------!
+    type(vector), intent(inout) :: v
+
+    v%val = 0.0_dp
+
+end subroutine vec_zero
+
+
+end module vectors
