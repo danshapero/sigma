bfe77c8de42160b4436f591ac51a24284f866d85
danshapero <shapero.daniel@gmail.com>
2014-04-08 21:17:05 -0700

    Got rid of linked list data type, which was unused

--- test/type_tests.f90
+++ test/type_tests.f90
@@ -4,33 +4,9 @@ use types
 
 implicit none
 
-    type(linked_list) :: list
     type(dynamic_array) :: a
     type(circular_array) :: c
     integer :: i,j
-
-
-    !----------------------------------------------------------------------!
-    ! Linked list tests                                                    !
-    !----------------------------------------------------------------------!
-    do i=1,10
-        call list%append(i)
-    enddo
-
-    i = list%get_entry(5)
-    if (i/=5) then
-        print *, 'list(5) should be = 5;'
-        print *, 'value found: ',i
-        call exit(1)
-    endif
-
-    call list%delete_entry(5)
-    i = list%get_value(5)
-    if (i/=6) then
-        print *, 'list(5) should now be = 5;'
-        print *, 'values found: ',i
-        call exit(1)
-    endif
 
 
     !----------------------------------------------------------------------!
