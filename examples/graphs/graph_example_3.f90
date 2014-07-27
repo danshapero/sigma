ddf8e9c747bca6d05d293b61fb19e375e35b240c
danshapero <shapero.daniel@gmail.com>
2014-07-27 11:54:26 -0700

    Examples now compile after refactor, need to check correctness

--- examples/graphs/graph_example_3.f90
+++ examples/graphs/graph_example_3.f90
@@ -20,7 +20,7 @@ implicit none
     real(dp) :: z(2), p
 
     ! integer indices
-    integer :: i,j,k,x,y,xs,ys,nx,ny
+    integer :: i, j, k, x, y, xs, ys, nx, ny
 
     ! variables for doing a depth-first search through the graph
     integer :: components, d
@@ -46,85 +46,78 @@ implicit none
     ! Set up a graph representing the integer lattice on a grid of         !
     ! nx x ny points.                                                      !
     !----------------------------------------------------------------------!
-    call g%init(nx*ny,degree=4)
+    call g%init(nx * ny)
 
     ! Make every (x,y) connected to (x,y+1), (x,y-1), (x+1,y) & (x-1,y)
-    do x = 1,nx
-        do y = 1,ny
-            i = ny*(x-1)+y
+    do x = 1, nx
+        do y = 1, ny
+            i = ny * (x - 1) + y
 
             ! Add edge from (x,y) to (x,y+1) and vice versa
-            j = ny*(x-1)+mod(y,ny)+1
-            call g%add_edge(i,j)
-            call g%add_edge(j,i)
+            j = ny * (x - 1) + mod(y, ny) + 1
+            call g%add_edge(i, j)
+            call g%add_edge(j, i)
 
             ! (x,y) to (x+1,y) and v.v.
-            j = ny*mod(x,nx)+y
-            call g%add_edge(i,j)
-            call g%add_edge(j,i)
+            j = ny * mod(x, nx) + y
+            call g%add_edge(i, j)
+            call g%add_edge(j, i)
         enddo
     enddo
 
     ! Delete all the connections between (x,1) and (x,1024)
-    do x=1,nx
-        i = ny*(x-1)+1
-        j = ny*(x-1)+ny
+    do x = 1, nx
+        i = ny * (x - 1) + 1
+        j = ny * (x - 1) + ny
 
-        call g%delete_edge(i,j)
-        call g%delete_edge(j,i)
+        call g%delete_edge(i, j)
+        call g%delete_edge(j, i)
     enddo
 
     ! Delete all the connections between (1,y) and (1024,y)
-    do y=1,ny
+    do y = 1, ny
         i = y
-        j = ny*(nx-1)+y
+        j = ny * (nx - 1) + y
 
-        call g%delete_edge(i,j)
-        call g%delete_edge(j,i)
+        call g%delete_edge(i, j)
+        call g%delete_edge(j, i)
     enddo
 
-    write(*,10) g%ne/2
-10  format('Done building initial graph;  ',i8,' edges.')
+    write(*,10) g%ne / 2
+10  format('Done building initial graph;  ', i8, ' edges.')
 
 
     !----------------------------------------------------------------------!
     ! Randomly delete edges.                                               !
     !----------------------------------------------------------------------!
-    do x=1,nx
-        do y=1,ny
+    do x = 1, nx
+        do y = 1, ny
             ! Harvest two random numbers
             call random_number(z)
 
             ! Find the index for the point (x,y)
-            i = ny*(x-1)+y
+            i = ny * (x - 1) + y
 
             ! If the first number is less than p, delete the connection
             ! to (x,y+1)
-            j = ny*(x-1)+mod(y,ny)+1
-            if (z(1)<p) then
-                call g%delete_edge(i,j)
-                call g%delete_edge(j,i)
+            j = ny * (x - 1) + mod(y, ny) + 1
+            if (z(1) < p) then
+                call g%delete_edge(i, j)
+                call g%delete_edge(j, i)
             endif
 
             ! If the second number is less than p, delete the connection
             ! to (x+1,y)
-            j = ny*mod(x,nx)+y
-            if (z(2)<p) then
-                call g%delete_edge(i,j)
-                call g%delete_edge(j,i)
+            j = ny * mod(x, nx) + y
+            if (z(2) < p) then
+                call g%delete_edge(i, j)
+                call g%delete_edge(j, i)
             endif
         enddo
     enddo
 
-    write(*,20) g%ne/2
-20  format('Done randomly removing edges; ',i8,' edges remain.')
-
-
-
-    !----------------------------------------------------------------------!
-    ! Compress the graph storage.                                          !
-    !----------------------------------------------------------------------!
-    call g%compress()
+    write(*,20) g%ne / 2
+20  format('Done randomly removing edges; ', i8, ' edges remain.')
 
 
 
@@ -137,19 +130,20 @@ implicit none
 
     ! Make an array `components` which will indicate which connected
     ! component of the graph each node belongs to
-    allocate(component(nx,ny),neighbors(g%max_degree))
+	d = g%max_degree()
+    allocate(component(nx, ny), neighbors(d))
     component = 0
 
-    do while(minval(component)==0)
+    do while(minval(component) == 0)
         call stack%init()
 
         ! Increment the number of components
-        components = components+1
+        components = components + 1
 
         ! Find a node which has not been assigned a component yet
-        do y=1,ny
-            do x=1,nx
-                if (component(x,y)==0) then
+        do y = 1, ny
+            do x = 1, nx
+                if (component(x, y) == 0) then
                     xs = x
                     ys = y
                 endif
@@ -157,10 +151,10 @@ implicit none
         enddo
 
         ! Compute the linear index i corresponding to the point (x,y)
-        i = nx*(xs-1)+ys
+        i = nx * (xs - 1) + ys
 
         ! Indicate that i belongs to the current component
-        component(xs,ys) = components
+        component(xs, ys) = components
 
         ! Push i onto the stack
         call stack%push(i)
@@ -174,20 +168,20 @@ implicit none
             d = g%degree(i)
 
             ! find all the neighbors of vertex i
-            call g%get_neighbors(neighbors,i)
+            call g%get_neighbors(neighbors, i)
 
             ! for each of those neighbors j,
-            do k=1,d
+            do k=1, d
                 j = neighbors(k)
 
                 ! find the (x,y) coordinate of j
-                y = mod(j,nx)+1
-                x = (j-y)/nx+1
+                y = mod(j, nx) + 1
+                x = (j - y) / nx + 1
 
                 ! check if vertex j has been visited yet
-                if (component(x,y)==0) then
+                if (component(x, y)==0) then
                     ! if not, mark it as belonging to the current component
-                    component(x,y) = components
+                    component(x, y) = components
 
                     ! then push it onto the stack
                     call stack%push(j)
@@ -200,7 +194,7 @@ implicit none
     enddo
 
     write(*,30) components
-30  format('Done traversing graph;       ',i8,' connected components.')
+30  format('Done traversing graph;       ', i8, ' connected components.')
 
 
 
@@ -210,23 +204,23 @@ implicit none
     !----------------------------------------------------------------------!
     allocate(top_components(nx), bottom_components(nx))
 
-    top_components = component(:,1)
-    bottom_components = component(:,ny)
+    top_components = component(:, 1)
+    bottom_components = component(:, ny)
 
     num_percolated = 0
-    do xt=1,nx
+    do xt = 1, nx
         comp = top_components(xt)
 
-        do xb=1,nx
-            if (bottom_components(xb)==comp) then
-                num_percolated = num_percolated+1
+        do xb = 1, nx
+            if (bottom_components(xb) == comp) then
+                num_percolated = num_percolated + 1
                 exit
             endif
         enddo
     enddo
 
-    write(*,40) (100.0_dp*num_percolated)/nx
-40  format(F12.6,'% of top sites percolated to the bottom.')
+    write(*,40) (100.0_dp * num_percolated) / nx
+40  format(F12.6, '% of top sites percolated to the bottom.')
 
 
 end program graph_example_3
--- examples/matrices/matrix_example_1.f90
+++ examples/matrices/matrix_example_1.f90
@@ -25,10 +25,10 @@ implicit none
     real(dp) :: z(512), p, entropy
 
     ! a sparse matrix
-    type(sparse_matrix) :: A
+    class(sparse_matrix), pointer :: A
 
     ! some integer indices
-    integer :: i,j,k
+    integer :: i, j, k
 
     ! variables for making the transition matrix corresponding to a Markov
     ! chain on g
@@ -42,7 +42,7 @@ implicit none
 
     ! initialize a random seed
     call init_seed()
-    p = 7.0/512
+    p = 7.0 / 512
 
     allocate(x(512), y(512))
 
@@ -54,22 +54,20 @@ implicit none
     allocate(ll_graph::g)
     call g%init(512)
 
-    do i=1,512
+    do i = 1, 512
         call random_number(z)
 
-        do j=1,512
-            if ( z(j)<p ) call g%add_edge(i,j)
+        do j = 1,512
+            if (z(j) < p) call g%add_edge(i, j)
         enddo
     enddo
 
-    do i=1,512
-        if (g%degree(i)==0) call g%add_edge(i,i)
+    do i = 1, 512
+        if (g%degree(i) == 0) call g%add_edge(i, i)
     enddo
 
-    ! Make g immutable
-    call g%compress()
-
-    write(*,10) g%max_degree
+	d = g%max_degree()
+    write(*,10) d
 10  format('Done generating random graph; max degree: ',i4)
 
 
@@ -78,24 +76,24 @@ implicit none
     !----------------------------------------------------------------------!
     ! Initialize a sparse matrix with 512 rows and columns, in row-major 
     ! ordering, with g representing its connectivity
-    call A%init(512,512,'row',g)
+    A => sparse_matrix(512, 512, g, 'row')
 
-    allocate(neighbors(g%max_degree))
+    allocate(neighbors(d))
 
     ! For each vertex i,
-    do i=1,512
+    do i = 1, 512
         ! compute the degree d of i
         d = g%degree(i)
 
         ! Find all the neighbors of i.
-        call g%get_neighbors(neighbors,i)
+        call g%get_neighbors(neighbors, i)
 
         ! For each neighbor j,
-        do k=1,d
+        do k = 1, d
             j = neighbors(k)
-            if (j/=0) then
+            if (j /= 0) then
                 ! Set A[i,j] = 1/degree(i)
-                call A%set_value(i,j,1.0_dp/d)
+                call A%set_value(i, j, 1.0_dp / d)
             endif
         enddo
     enddo
@@ -111,8 +109,8 @@ implicit none
     ! Compute the matrix-vector product,  y = A*x
     call A%matvec(x,y)
 
-    write(*,20) minval(y)-1.0_dp,maxval(y)-1.0_dp
-20  format('Rows sum to 1 to within error (',e12.6,',',e12.6,')')
+    write(*,20) minval(y) - 1.0_dp, maxval(y) - 1.0_dp
+20  format('Rows sum to 1 to within error (', e12.6, ',', e12.6, ')')
 
 
 
@@ -123,24 +121,24 @@ implicit none
     x = 0.0
     x(1) = 1.0
 
-    do k=1,2048
-        call A%matvec_t(x,y)
-        x = y/sum(y)
+    do k = 1, 2048
+        call A%matvec_t(x, y)
+        x = y / sum(y)
     enddo
 
     entropy = 0.0
-    do i=1,512
-        if (x(i)/=0) entropy = entropy-x(i)*log(x(i))
+    do i = 1, 512
+        if (x(i) /= 0) entropy = entropy - x(i) * log(x(i))
     enddo
 
-    write(*,30) 100*entropy/log(512.0)
+    write(*,30) 100*entropy / log(512.0)
 30  format('After 1024 iterations, entropy relative maximum is ',f9.6,'%.')
 
-    call A%matvec_t(x,y)
+    call A%matvec_t(x, y)
     write(*,40)
 40  format('Residual of invariant distribution as left eigenvector')
-    write(*,50) maxval(dabs(x-y))
-50  format('   of transition matrix: ',e12.6)
+    write(*,50) maxval(dabs(x - y))
+50  format('   of transition matrix: ', e12.6)
 
 
 end program matrix_example_1
