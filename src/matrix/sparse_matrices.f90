bba5a8850c5fb283541f908203df57e010da7c80
danshapero <shapero.daniel@gmail.com>
2014-07-30 10:29:22 -0700

    Moved sparse matrix sum helper routine to sparse_matrices module, added procedure to fill entries

--- src/matrix/sparse_matrices.f90
+++ src/matrix/sparse_matrices.f90
@@ -44,5 +44,112 @@ end function sparse_matrix_factory
 
 
 
+!==========================================================================!
+!==== Algebraic operations on sparse matrices                          ====!
+!==========================================================================!
+
+!--------------------------------------------------------------------------!
+subroutine sparse_matrix_sum_graph(g, B, C)                                !
+!--------------------------------------------------------------------------!
+!     This routines computes the graph `g` describing the underlying       !
+! connectivity structure of the sparse matrix sum `B + C`. It is a helper  !
+! routine for other procedures which compute the entries of the sum.       !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(graph), intent(inout) :: g
+    class(sparse_matrix), intent(in) :: B, C
+    ! local variables
+    integer :: i, j, k
+    integer :: n, num_batches, num_returned, edges(2, batch_size)
+    type(graph_edge_cursor) :: cursor
+
+    call g%init(B%nrow, B%ncol)
+
+
+    ! Add all the edges from `B` into `g`
+    cursor = B%make_cursor()
+    num_batches = (cursor%final - cursor%start) / batch_size + 1
+
+    do n = 1, num_batches
+        call B%get_edges(edges, cursor, batch_size, num_returned)
+
+        do k = 1, num_returned
+            i = edges(1, k)
+            j = edges(2, k)
+
+            call g%add_edge(i, j)
+        enddo
+    enddo
+
+
+    ! Now add all the edges from `C` into `g`
+    cursor = C%make_cursor()
+    num_batches = (cursor%final - cursor%start) / batch_size + 1
+
+    do n = 1, num_batches
+        call C%get_edges(edges, cursor, batch_size, num_returned)
+
+        do k = 1, num_returned
+            i = edges(1, k)
+            j = edges(2, k)
+
+            call g%add_edge(i, j)
+        enddo
+    enddo
+
+end subroutine sparse_matrix_sum_graph
+
+
+
+!--------------------------------------------------------------------------!
+subroutine sparse_matrix_sum_fill_entries(A, B, C)                         !
+!--------------------------------------------------------------------------!
+    ! input/output variables
+    class(sparse_matrix), intent(inout) :: A
+    class(sparse_matrix), intent(in)    :: B, C
+    ! local variables
+    integer :: i, j, k
+    real(dp) :: z
+    integer :: n, num_batches, num_returned, edges(2, batch_size)
+    real(dp) :: entries(batch_size)
+    type(graph_edge_cursor) :: cursor
+
+    ! Add up the contributions to `A` from `B`
+    cursor = B%make_cursor()
+    num_batches = (cursor%final - cursor%start) / batch_size + 1
+
+    do n = 1, num_batches
+        call B%get_entries(edges, entries, cursor, batch_size, num_returned)
+
+        do k = 1, num_returned
+            i = edges(1, k)
+            j = edges(2, k)
+            z = entries(k)
+
+            call A%add_value(i, j, z)
+        enddo
+    enddo
+
+    ! Add up the contributions to `A` from `C`
+    cursor = C%make_cursor()
+    num_batches = (cursor%final - cursor%start) / batch_size + 1
+
+    do n = 1, num_batches
+        call C%get_entries(edges, entries, cursor, batch_size, num_returned)
+
+        do k = 1, num_returned
+            i = edges(1, k)
+            j = edges(2, k)
+            z = entries(k)
+
+            call A%add_value(i, j, z)
+        enddo
+    enddo
+
+end subroutine sparse_matrix_sum_fill_entries
+
+
+
+
 
 end module sparse_matrices
