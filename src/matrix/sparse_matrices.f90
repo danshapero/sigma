750b86d684bb538824326d66469afb8d080f29b0
danshapero <shapero.daniel@gmail.com>
2014-09-10 11:42:25 -0700

    Moved the factory routines into their own module

--- src/matrix/sparse_matrices.f90
+++ src/matrix/sparse_matrices.f90
@@ -4,11 +4,7 @@ module sparse_matrices                                                     !
 !==========================================================================!
 !==========================================================================!
 !====     This module contains data types and routines that wrap all   ====!
-!==== the atomic sparse matrix types and operations. This includes     ====!
-!==== factory methods and the wrapper type sparse_matrix.              ====!
-!====     Factory methods take in a string or integer describing the   ====!
-!==== name of a sparse matrix storage format and return a pointer to a ====!
-!==== sparse matrix in that format.                                    ====!
+!==== the atomic sparse matrix types and operations.                   ====!
 !====     The sparse_matrix type is used as a wrapper around all the   ====!
 !==== basic matrix types. It contains an array of polymorphic pointers ====!
 !==== to other sparse matrices. Its simplest use is as a wrapper       ====!
@@ -34,6 +30,7 @@ use sparse_matrix_interfaces
 use default_matrices
 use cs_matrices
 use ellpack_matrices
+use sparse_matrix_factory
 use sparse_matrix_algebra, only: sparse_matrix_sum, sparse_matrix_product
 
 implicit none
@@ -124,26 +121,8 @@ end type sparse_matrix
 
 
 
-! Parameter giving the number of different matrix formats available.
-! Test programs iterate over every matrix type to check correctness of each
-! matrix implementation.
-integer, parameter :: num_matrix_types = 5
-
-
-! Overload the matrix factory routines
-interface choose_matrix_type
-    module procedure choose_matrix_type_by_int, choose_matrix_type_by_name
-end interface
-
 contains
 
-
-
-!==========================================================================!
-!========                                                          ========!
-!====               Methods for sparse_matrix objects                  ====!
-!========                                                          ========!
-!==========================================================================!
 
 
 !==========================================================================!
@@ -753,74 +732,6 @@ elemental function composite_mat_get_owning_colmat(A, j) result(jt)        !
     enddo
 
 end function composite_mat_get_owning_colmat
-
-
-
-
-
-!==========================================================================!
-!========                                                          ========!
-!====                    Matrix factory methods                        ====!
-!========                                                          ========!
-!==========================================================================!
-
-
-!--------------------------------------------------------------------------!
-subroutine choose_matrix_type_by_int(A, t)                                 !
-!--------------------------------------------------------------------------!
-    class(sparse_matrix_interface), pointer, intent(inout) :: A
-    integer, intent(in) :: t
-
-    nullify(A)
-
-    select case(t)
-        case(1)
-            allocate(default_row_matrix :: A)
-
-        case(2)
-            allocate(default_column_matrix :: A)
-
-        case(3)
-            allocate(csr_matrix :: A)
-
-        case(4)
-            allocate(csc_matrix :: A)
-
-        case(5)
-            allocate(ellpack_matrix :: A)
-
-    end select
-
-end subroutine choose_matrix_type_by_int
-
-
-
-!--------------------------------------------------------------------------!
-subroutine choose_matrix_type_by_name(A, frmt)                             !
-!--------------------------------------------------------------------------!
-    class(sparse_matrix_interface), pointer, intent(inout) :: A
-    character(len=*), intent(in) :: frmt
-
-    nullify(A)
-
-    select case( trim(frmt) )
-        case("default row")
-            allocate(default_row_matrix :: A)
-
-        case("default column", "default col")
-            allocate(default_column_matrix :: A)
-
-        case("csr", "crs", "harwell-boeing", "compressed sparse row")
-            allocate(csr_matrix :: A)
-
-        case("csc", "ccs", "compressed sparse column")
-            allocate(csc_matrix :: A)
-
-        case("ellpack", "itpack")
-            allocate(ellpack_matrix :: A)
-    end select
-
-end subroutine choose_matrix_type_by_name
 
 
 
